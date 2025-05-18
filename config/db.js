dd618dd93c077b749609da9a150701f168a97e7a
mythsoul <mythsoul2023@gmail.com>
2025-05-18 09:00:00 +0545

    Add database configuration

---
+++ config/db.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+      maxPoolSize: 10,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+      family: 4,
+      retryWrites: true,
+    };
+
+    mongoose.set('strictQuery', true);
+    const uri = process.env.MONGODB_URI;
+    const dbName = '/quickcart'; 
+    cached.promise = mongoose.connect(`${uri}${dbName}`, opts).then((mongoose) => {
+      console.log('MongoDB connected to test database');
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+export default connectDB;
